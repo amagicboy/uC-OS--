6aecac3231b733e489a350925e5a3a274d4e434b
xiami <amagicowboy@gmai.com>
2021-03-16 17:50:48 +0800

    create_Task

--- User/app.c
+++ User/app.c
@@ -10,6 +10,22 @@ static CPU_STK Task2Stk[TASK2_STK_SIZE];
 uint32_t flag1;
 uint32_t flag2;
 
+void delay(uint32_t count)
+{
+	for(;count!=0;count--);
+}
+
+void Task1(void *p_arg)
+{
+	for(;;)
+	{
+		flag1 = 1;
+		delay(100);
+		flag1 = 0;
+		delay(100);
+	}
+}
+
 int main(void)
 {
 
